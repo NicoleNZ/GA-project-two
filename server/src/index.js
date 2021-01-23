ae49c3ab11ba83616cf1d1ab10fd5e7d699e621f
Nicole <nicole.barnett.nz@gmail.com>
2021-01-23 13:41:57 +1300

    FINALLY found the issue... Internal routes now working in Postman

--- server/src/index.js
+++ server/src/index.js
@@ -1,36 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const session = require("express-session");
-
-const internalRouter = require("./routes/internalRouter");
-const userRouter = require("./routes/userRouter");
-const appRouter = require("./routes/vaultRouter");
 
 mongoose.connect("mongodb://localhost:27017/masterDataVault", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    });
+});
 
 const app = express();
-
 const port = 4000;
+
+const internalRouter = require("./routes/internalRoutes");
+const userRouter = require("./routes/userRoutes");
+const vaultRouter = require("./routes/vaultRoutes");
 
 app.use(express.json());
 app.use(cors());
-app.use(
-    session({
-    secret: "secret vault",
-    resave: false,
-    saveUninitialized: false,
-    })
-);
 
 app.use("/internal", internalRouter);
-app.use("/api/vault", vaultRouter);
-app.use("/api/users", userRouter);
 
-app.listen(port, () =>
-    console.log(`Fruit app is listening at http://localhost:${port}`)
-);
 
+app.listen(port, () => {
+    console.log(`App is listening at http://localhost:${port}`);
+});
